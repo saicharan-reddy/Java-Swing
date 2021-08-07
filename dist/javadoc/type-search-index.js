34ff4a3ef30ab8609f746969384a93156791efb5
saicharan-reddy <54852188+saicharan-reddy@users.noreply.github.com>
2021-08-07 08:29:20 +0530

    Add files via upload

---
+++ dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"controller","l":"ButtonController"},{"p":"view","l":"ButtonPanel"},{"p":"view","l":"Field"},{"p":"model","l":"Game"},{"p":"view","l":"Sudoku"},{"p":"controller","l":"SudokuController"},{"p":"view","l":"SudokuPanel"},{"p":"model","l":"UpdateAction"}];updateSearchResults();
---
+++ src/model/UpdateAction.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ * Enumeration used to inform observers what to update.
+ * @author sai charan reddy gogireddy
+ */
+
+public enum UpdateAction {
+    NEW_GAME,
+    CHECK,
+    SELECTED_NUMBER,
+    CANDIDATES,
+    HELP
+}
